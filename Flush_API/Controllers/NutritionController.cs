ec356b6c4fa49d7f42e2999022b4965afc8ef12e
zep1994 <thomasmatlockbba@gmail.com>
2023-07-16 22:31:25 -0500

    begin controller api

---
+++ Flush_API/Controllers/NutritionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Flush_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NutritionController : ControllerBase
+    {
+        // GET: api/<NutritionController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<NutritionController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NutritionController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NutritionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NutritionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
